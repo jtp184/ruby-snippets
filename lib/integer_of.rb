51887cc11a7666ff7bc2249196db8cc22b8fb283
Justin Piotroski <justin.piotroski@gmail.com>
2019-03-12 22:28:10 -0700

    Using different methods based on block or element in Integer#of

--- lib/integer_of.rb
+++ lib/integer_of.rb
@@ -1,12 +1,10 @@
 # Addes an #of method to integers, taking either a single element, or a block
 class Integer
   def of(ele=nil, &blk)
-    r = []
     if ele
-      self.times { r << ele }
+      self * [ele]
     elsif block_given?
-      self.times { r << blk.call }
+      self.times.with_object([]) { |_t, r| r << blk.call }
     end
-    r
   end
 end
