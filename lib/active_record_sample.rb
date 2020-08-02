a3a1d1a825e9143144742f1c97bbb68b2dc0f666
Justin Piotroski <justin.piotroski@gmail.com>
2020-08-02 16:11:22 -0700

    Smart db switching, update to return value logic

--- lib/active_record_sample.rb
+++ lib/active_record_sample.rb
@@ -5,7 +5,21 @@
 # Different databases use different random functions so YMMV
 class ActiveRecord::Base
   def self.sample(n=1)
-    r = self.order('random()').limit(n)
-    r.one? ? r.first : r
+    r = self.order(db_random_function).limit(n)
+    n == 1 ? r.first : r
+  end
+  
+  def self.db_random_function
+    return @db_random_function if @db_random_function
+    
+    app = Rails.instance_variable_get(:@application)
+    db = app.config.database_configuration[Rails.env]['adapter']
+    
+    @db_random_function = case db
+                          when 'mysql2'
+                            'RAND()'
+                          else
+                            'random()'
+                          end
   end
 end
